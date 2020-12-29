a011f55cc938811d678aa80fb4958b8b52ef1f1c
TkNikita <tkachenok-nikita@mail.ru>
2020-12-29 04:13:12 +0300

    ...

--- src/main/java/ua/com/foxminded/classtimetable/Application.java
+++ src/main/java/ua/com/foxminded/classtimetable/Application.java
@@ -1,9 +1,35 @@
 package ua.com.foxminded.classtimetable;
 
+import java.io.IOException;
+import java.net.URISyntaxException;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import ua.com.foxminded.classtimetable.config.DBConfiguratoin;
+import ua.com.foxminded.classtimetable.dao.BuildingDao;
+import ua.com.foxminded.classtimetable.database.DBManager;
+import ua.com.foxminded.classtimetable.entities.Building;
+
 public class Application {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException, URISyntaxException {
 
+		try (AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(
+				DBConfiguratoin.class);) {
+			DBManager manager = context.getBean(DBManager.class);
+			manager.createTables();
+			BuildingDao bDao = context.getBean(BuildingDao.class);
+			Building building = new Building();
+			building.setBuildingName("First corpus");
+			bDao.save(building);
+			System.out.println(bDao.getById(1));
+			building.setId(1);
+			building.setBuildingName("Second corpus");
+			bDao.update(building);
+			System.out.println(bDao.getById(1));
+			bDao.delete(building);
+			System.out.println(bDao.getAll());
+		}
 	}
 
 }
